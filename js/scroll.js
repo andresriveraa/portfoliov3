cda3ab9c86c72c86d4d9f75491b0bf498a1bfa1f
Circuandante <circuandante@gmail.com>
2021-03-06 09:02:40 -0500

    feat smooth scroll and horizontal scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,14 @@
 
 
-let wrap = document.getElementById('wrap');
-let header = document.getElementsByTagName('header');
-let main = document.getElementsByTagName('main');
+ const widthScre = screen.width / 120
+ const scroll = new LocomotiveScroll({
+   el: document.querySelector('[data-scroll-container]'),
+   smooth: true,
+   direction: 'horizontal',
+   multiplier: widthScre
+ });
+
+const animationScroll = (e) => {
+  if (e.deltaY > 0) console.log('mayor');
+  else if (e.deltaY < 0) console.log('menor');
+}
