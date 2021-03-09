ab1937c331c8e281e1979ef10c132872acbea778
Circuandante <circuandante@gmail.com>
2021-03-09 18:46:05 -0500

    Design add animation load

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+const App = document.getElementById('app')
+const imgHero = document.getElementById('imgHero')
+
+const Load = document.getElementById('Load')
+
+window.addEventListener("load", function(event) {
+  App.classList.remove('loadHide')
+  Load.style.display = 'none'  
+});
