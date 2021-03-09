aeee67a042d6e540108631d96d80c687dc72a915
Circuandante <circuandante@gmail.com>
2021-03-09 09:48:23 -0500

    design finish laptop view

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,13 @@
 
-
- const widthScre = screen.width / 120
- const scroll = new LocomotiveScroll({
-   el: document.querySelector('[data-scroll-container]'),
-   smooth: true,
-   direction: 'horizontal',
-   multiplier: widthScre
- });
+const screenWidht = screen.width || 1366
+const multiplier = screenWidht / 120
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+  direction: 'horizontal',
+  lerp: 0.05,
+  multiplier,
+});
 
 const animationScroll = (e) => {
   if (e.deltaY > 0) console.log('mayor');
