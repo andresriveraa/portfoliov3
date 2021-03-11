1eea0e34fe12ec52c882af507d41b62988289dbc
Circuandante <circuandante@gmail.com>
2021-03-11 15:38:51 -0500

    fixed some advice lighthouse

--- js/animation.js
+++ js/animation.js
@@ -4,7 +4,8 @@ const imgHero = document.getElementById('imgHero')
 const Load = document.getElementById('Load')
 
 window.addEventListener("load", function(event) {
-  imgHero.src = 'http://www.andresrivera.com.co/assets/img/Andres-full.png'
+  // imgHero.src = 'http://www.andresrivera.com.co/assets/img/Andres-full.png'
+  imgHero.src = '/assets/img/Andres-full.png'
   App.classList.remove('loadHide')
   Load.style.display = 'none'  
 });
