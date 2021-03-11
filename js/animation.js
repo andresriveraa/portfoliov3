d4994a5340dedcaa4b16ef99c679a399115eb57f
Circuandante <circuandante@gmail.com>
2021-03-11 13:16:50 -0500

    image not load

--- js/animation.js
+++ js/animation.js
@@ -4,7 +4,7 @@ const imgHero = document.getElementById('imgHero')
 const Load = document.getElementById('Load')
 
 window.addEventListener("load", function(event) {
-  imgHero.src = 'http://www.andresrivera.com.co/assets/img/Andres.png'
+  imgHero.src = 'http://www.andresrivera.com.co/assets/img/Andres-full.png'
   App.classList.remove('loadHide')
   Load.style.display = 'none'  
 });
